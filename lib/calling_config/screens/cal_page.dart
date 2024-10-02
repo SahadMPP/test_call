fe7222e85775480493222ca782b98238559225eb
Sahad mp <muhdsahad4916@gmail.com>
2024-10-02 15:57:44 +0530

    calling page ui

--- lib/calling_config/screens/cal_page.dart
+++ lib/calling_config/screens/cal_page.dart
@@ -13,60 +13,59 @@ class CallView extends StatefulWidget {
 }
 
 class _CallViewState extends State<CallView> {
-
- String roomIdd = '';
+  String roomIdd = '';
 
   void startListenEvent() {
-  // Callback for updates on the status of other users in the room.
-  // Users can only receive callbacks when the isUserStatusNotify property of ZegoRoomConfig is set to `true` when logging in to the room (loginRoom).
-  ZegoExpressEngine.onRoomUserUpdate = (roomID, updateType, List<ZegoUser> userList) {
-    (
-        'onRoomUserUpdate: roomID: $roomID, updateType: ${updateType.name}, userList: ${userList.map((e) => e.userID)}');
-  };
-  // Callback for updates on the status of the streams in the room.
-  ZegoExpressEngine.onRoomStreamUpdate = (roomID, updateType, List<ZegoStream> streamList, extendedData) {
-    (
-        'onRoomStreamUpdate: roomID: $roomID, updateType: $updateType, streamList: ${streamList.map((e) => e.streamID)}, extendedData: $extendedData');
-    if (updateType == ZegoUpdateType.Add) {
-      for (final stream in streamList) {
-        startPlayStream(stream.streamID);
+    // Callback for updates on the status of other users in the room.
+    // Users can only receive callbacks when the isUserStatusNotify property of ZegoRoomConfig is set to `true` when logging in to the room (loginRoom).
+    ZegoExpressEngine.onRoomUserUpdate =
+        (roomID, updateType, List<ZegoUser> userList) {
+      ('onRoomUserUpdate: roomID: $roomID, updateType: ${updateType.name}, userList: ${userList.map((e) => e.userID)}');
+    };
+    // Callback for updates on the status of the streams in the room.
+    ZegoExpressEngine.onRoomStreamUpdate =
+        (roomID, updateType, List<ZegoStream> streamList, extendedData) {
+      ('onRoomStreamUpdate: roomID: $roomID, updateType: $updateType, streamList: ${streamList.map((e) => e.streamID)}, extendedData: $extendedData');
+      if (updateType == ZegoUpdateType.Add) {
+        for (final stream in streamList) {
+          startPlayStream(stream.streamID);
+        }
+      } else {
+        for (final stream in streamList) {
+          stopPlayStream(stream.streamID);
+        }
       }
-    } else {
-      for (final stream in streamList) {
-        stopPlayStream(stream.streamID);
-      }
-    }
-  };
-  // Callback for updates on the current user's room connection status.
-  ZegoExpressEngine.onRoomStateUpdate = (roomID, state, errorCode, extendedData) {
-    (
-        'onRoomStateUpdate: roomID: $roomID, state: ${state.name}, errorCode: $errorCode, extendedData: $extendedData');
-  };
+    };
+    // Callback for updates on the current user's room connection status.
+    ZegoExpressEngine.onRoomStateUpdate =
+        (roomID, state, errorCode, extendedData) {
+      ('onRoomStateUpdate: roomID: $roomID, state: ${state.name}, errorCode: $errorCode, extendedData: $extendedData');
+    };
 
-  // Callback for updates on the current user's stream publishing changes.
-  ZegoExpressEngine.onPublisherStateUpdate = (streamID, state, errorCode, extendedData) {
-    (
-        'onPublisherStateUpdate: streamID: $streamID, state: ${state.name}, errorCode: $errorCode, extendedData: $extendedData');
-  };
-}
+    // Callback for updates on the current user's stream publishing changes.
+    ZegoExpressEngine.onPublisherStateUpdate =
+        (streamID, state, errorCode, extendedData) {
+      ('onPublisherStateUpdate: streamID: $streamID, state: ${state.name}, errorCode: $errorCode, extendedData: $extendedData');
+    };
+  }
 
-Future<void> startPlayStream(String streamID) async {
-  // Start to play streams.
-  ZegoExpressEngine.instance.startPlayingStream(streamID);
-}
+  Future<void> startPlayStream(String streamID) async {
+    // Start to play streams.
+    ZegoExpressEngine.instance.startPlayingStream(streamID);
+  }
 
-Future<void> stopPlayStream(String streamID) async {
-  ZegoExpressEngine.instance.stopPlayingStream(streamID);
-}
+  Future<void> stopPlayStream(String streamID) async {
+    ZegoExpressEngine.instance.stopPlayingStream(streamID);
+  }
 
-void stopListenEvent() {
-  ZegoExpressEngine.onRoomUserUpdate = null;
-  ZegoExpressEngine.onRoomStreamUpdate = null;
-  ZegoExpressEngine.onRoomStateUpdate = null;
-  ZegoExpressEngine.onPublisherStateUpdate = null;
-}
+  void stopListenEvent() {
+    ZegoExpressEngine.onRoomUserUpdate = null;
+    ZegoExpressEngine.onRoomStreamUpdate = null;
+    ZegoExpressEngine.onRoomStateUpdate = null;
+    ZegoExpressEngine.onPublisherStateUpdate = null;
+  }
 
- //Create room Id
+  //Create room Id
   String getRoomId() {
     // Random rand = Random();
     if (widget.roomId == null) {
@@ -87,49 +86,51 @@ void stopListenEvent() {
     return rand.nextInt(100000000).toString();
   }
 
-  
-Future<ZegoRoomLoginResult> loginRoom(String userID, String userNm) async {
-  // The value of `userID` is generated locally and must be globally unique.
-  final user = ZegoUser(userID, userNm);
+  Future<ZegoRoomLoginResult> loginRoom(String userID, String userNm) async {
+    // The value of `userID` is generated locally and must be globally unique.
+    final user = ZegoUser(userID, userNm);
 
-  // The value of `roomID` is generated locally and must be globally unique.
-   roomIdd = getRoomId();
+    // The value of `roomID` is generated locally and must be globally unique.
+    roomIdd = getRoomId();
 
-  // onRoomUserUpdate callback can be received when "isUserStatusNotify" parameter value is "true".
-  ZegoRoomConfig roomConfig = ZegoRoomConfig.defaultConfig()..isUserStatusNotify = true;
+    // onRoomUserUpdate callback can be received when "isUserStatusNotify" parameter value is "true".
+    ZegoRoomConfig roomConfig = ZegoRoomConfig.defaultConfig()
+      ..isUserStatusNotify = true;
 
-  // log in to a room
-  // Users must log in to the same room to call each other.
-  return ZegoExpressEngine.instance.loginRoom(roomIdd, user, config: roomConfig).then((ZegoRoomLoginResult loginRoomResult) {
-    ('loginRoom: errorCode:${loginRoomResult.errorCode}, extendedData:${loginRoomResult.extendedData}');
-    if (loginRoomResult.errorCode == 0) {
-      // startPreview();
-      startPublish();
-    } else {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text('loginRoom failed: ${loginRoomResult.errorCode}')));
-    }
-    return loginRoomResult;
-  });
-}
+    // log in to a room
+    // Users must log in to the same room to call each other.
+    return ZegoExpressEngine.instance
+        .loginRoom(roomIdd, user, config: roomConfig)
+        .then((ZegoRoomLoginResult loginRoomResult) {
+      ('loginRoom: errorCode:${loginRoomResult.errorCode}, extendedData:${loginRoomResult.extendedData}');
+      if (loginRoomResult.errorCode == 0) {
+        // startPreview();
+        startPublish();
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('loginRoom failed: ${loginRoomResult.errorCode}')));
+      }
+      return loginRoomResult;
+    });
+  }
 
-Future<ZegoRoomLogoutResult> logoutRoom() async {
-  // stopPreview();
-  stopPublish();
-  return ZegoExpressEngine.instance.logoutRoom(roomIdd);
-}
+  Future<ZegoRoomLogoutResult> logoutRoom() async {
+    // stopPreview();
+    stopPublish();
+    return ZegoExpressEngine.instance.logoutRoom(roomIdd);
+  }
 
-Future<void> startPublish() async{
-  // After calling the `loginRoom` method, call this method to publish streams.
-  // The StreamID must be unique in the room.
-  String streamID = 'rooom_${widget.name}_call';
-  ZegoExpressEngine.instance.enableCamera(false);
-  return ZegoExpressEngine.instance.startPublishingStream(streamID);
-}
+  Future<void> startPublish() async {
+    // After calling the `loginRoom` method, call this method to publish streams.
+    // The StreamID must be unique in the room.
+    String streamID = 'rooom_${widget.name}_call';
+    ZegoExpressEngine.instance.enableCamera(false);
+    return ZegoExpressEngine.instance.startPublishingStream(streamID);
+  }
 
-Future<void> stopPublish() async {
-  return ZegoExpressEngine.instance.stopPublishingStream();
-}
+  Future<void> stopPublish() async {
+    return ZegoExpressEngine.instance.stopPublishingStream();
+  }
 
   @override
   void initState() {
@@ -138,8 +139,7 @@ Future<void> stopPublish() async {
     loginRoom(getUserId(), widget.name!);
   }
 
-
-@override
+  @override
   void dispose() {
     super.dispose();
     stopListenEvent();
@@ -147,21 +147,61 @@ Future<void> stopPublish() async {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Column(
-        children: [
-          const Spacer(),
-          Text('User Name : ${widget.name}'),
-          CircleAvatar(
-            backgroundColor: Colors.red,
-            child: IconButton(onPressed: (){
-              logoutRoom().then((value) {
-                Navigator.of(context).pop();
-              },);
-            }, icon: const Icon(Icons.call,color: Colors.white,)),
-          ),
-          const SizedBox(height: 100)
-        ],
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('asset/Annotation 2024-10-02 153846.png'),
+                fit: BoxFit.cover)),
+        child: Column(
+          children: [
+            const Spacer(
+              flex: 2,
+            ),
+            const CircleAvatar(
+              radius: 70,
+              backgroundImage: NetworkImage(
+                  'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?t=st=1727862910~exp=1727866510~hmac=a6e81175b4977bc951c9a7679eab70bbd9c3b82d3ba5d97bfdf87ad791f8d1bf&w=740'),
+            ),
+            const SizedBox(height: 10),
+            const Text('Sanusha'),
+            const Spacer(),
+            ElevatedButton(onPressed: () {}, child: const Text('Add Coin')),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Colors.red,
+                ),
+                CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Colors.red,
+                  child: IconButton(
+                      onPressed: () {
+                        logoutRoom().then(
+                          (value) {
+                            Navigator.of(context).pop();
+                          },
+                        );
+                      },
+                      icon: const Icon(
+                        Icons.call,
+                        color: Colors.white,
+                      )),
+                ),
+                const CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Colors.red,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 50,
+            )
+          ],
+        ),
       ),
     );
   }
